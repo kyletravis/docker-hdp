ef74fe088f7b05087bb30c3f66cf09501072855c
Kyle Travis <kyle@kyletravis.com>
2016-07-12 21:55:09 -0500

    updated backup script

--- scripts/backup-images.sh
+++ scripts/backup-images.sh
@@ -1,3 +1,3 @@
 docker save node | pigz > images/node.tar.gz
-docker save ambari-server | pigz > ambari-server.tar.gz
-docker save pgsql | pigz > pgsql.tar.gz
+docker save ambari-server | pigz > images/ambari-server.tar.gz
+docker save pgsql | pigz > images/pgsql.tar.gz
